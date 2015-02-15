dc70702988c3e99fdf165c0154882f7cb2f5dbfa
Azdean Samih <azdean.samih@gmail.com>
2015-02-15 23:32:07 +0000

    Inputs, styling and more
    Each task to add to the graph will have an input node. Created a JS
    function of adding nodes. Created and styled node forms.

--- Chartor/index.php
+++ Chartor/index.php
@@ -2,7 +2,14 @@
 	include 'assets/framework/header.php';
 ?>
 <section id="content">
-	<p>Content</p>
+	
+<aside id="inputArea">
+	<div id="inputTitle"><h1>Add nodes here!</h1><img id="addNode" src="assets/img/add.svg"></img></div>
+		
+		
+			<!--<button type="submit">Submit</button>-->
+</aside>
+
 </section>
 <?php
 	include 'assets/framework/footer.php';
