6ecddefc70e4013e74323115da1ff62f5cd41793
Andrew Stanton <up683693@myport.ac.uk>
2015-02-15 14:44:28 +0000

    newNode() and dependants prototype done. TODO invocation with data and connectors.

--- wbt/chartor.js
+++ wbt/chartor.js
@@ -12,19 +12,34 @@ function connectionPoint (elem, isBtm) {
     return [x, y];
 }
 
+function newRectComponent (parent, x, y) {
+    console.log("Making rect");
+    var myRect = document.createElementNS(svgNS,"rect");
+    myRect.setAttribute("id","myRectangle");
+    myRect.setAttribute("x", x);
+    myRect.setAttribute("y", y);
+    myRect.setAttribute("width", 200);
+    myRect.setAttribute("height", 100);
+    myRect.setAttribute("fill","red");
+    myRect.setAttribute("stroke","none");
+    parent.appendChild(myRect);
+}
+
 function newTextComponent (parent, x, y, text) {
     // Add a text element to the node. Giving , location and text content
-    // var component = document.createElement("text");
-    var component = document.createElementNS(svgNS, "text");
-    parent.appendChild(component);
-    component.setAttribute("x", x);
-    component.setAttribute("y", y);
-    component.textContent = text;
+
+    var myText = document.createElementNS(svgNS,"text");
+    myText.setAttribute("id","myText");
+    myText.setAttribute("x", x);
+    myText.setAttribute("y", y);
+    myText.textContent = text; 
+    parent.appendChild(myText);
 }
 
 function newNode (number, name, x, y) {
     // console.log("In function newNode");
-    var g = document.createElement('g');
+    var g = document.createElementNS(svgNS, 'g');
+    newRectComponent(g, x, y);
     newTextComponent(g, x +20, y +40, number);
     newTextComponent(g, x +20, y +60, name);
     // console.log(g);
