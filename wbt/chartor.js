7254ffcc50a07bd0d6c7ed5f516a09dd7369c5de
Andrew Stanton <up683693@myport.ac.uk>
2015-02-16 01:23:51 +0000

    (js) pseudo code. NOT TESTED

--- wbt/chartor.js
+++ wbt/chartor.js
@@ -5,10 +5,12 @@ function getAttrAsInt (elem, attr) {
     return parseInt(elem.getAttribute(attr));
 }
 
-function connectionPoint (elem, isBtm) {
-    var x,y;
+function connectionPoint (node, isParent) {
+    // Given node, get rect and determine were to connect lines to. If parent node, connection at bottom; else top.
+    var x,y, elem;
+    elem = node.children[0];    // Need to use rectangle as g has no dimension attrs to read.
     x = getAttrAsInt(elem, "x") + (getAttrAsInt(elem, "width")/2)
-    y = getAttrAsInt(elem, "y") + isBtm ? getAttrAsInt(elem, "height") : 0;
+    y = getAttrAsInt(elem, "y") + isParent ? getAttrAsInt(elem, "height") : 0;
     return [x, y];
 }
 
@@ -45,33 +47,35 @@ function newNode (number, name, x, y) {
     return g;
 }
 
-function determineNodes(data) {
-    // this element
-    // for atr in childNodes:
-        // recurse returning node ident for produced childNodes
-        // use ident to link to parent
+function determineNodes(svgElem, data, number, x, y) {
+    // Recursive function to oversee node creation
+    var parent, parentConnection,
+    childrenConnections = [],
+    name = data.name,
+    children = data.children,
+
+    recurse = function (element, index, array) {
+        // Inner function to use forEach method to ensure order
+        var num = number + "." + toString(index + 1);
+        childrenConnections.push(determineNodes(svgElem, element, num, x, y));
+    };
+
+    parent = newNode(number, name, x, y);
+    svgElem.appendChild(n);
+    parentConnection = connectionPoint(parent, true);
+    console.log(parentConnection);
+
+    children.forEach(recurse);  // Using forEach method as other form doesn't ensure order.
+
+    return parent;
 }
 
-function buildWBT (svgElem, data) {
-    // Given svg elem, and data call newNode to create the nodes. Then link them up.
+function buildWBT (data) {
+    // Gets svg element, and data then calls determineNodes to create the nodes and link them up.
+
+    var svgElem, data;
+    svgElem = document.getElementById("svg");
+
 
     var n = newNode(1, "Root", 0, 0);
-    // console.log(n);
-    var svg = document.getElementById("svg");
-    // console.log(svg);
-    svg.appendChild(n);
-    // return n
-
-
-    // TODO:
-    //  Unpack data for current row
-    //  Call newNode with data
-    //  Link parent to children
-    //  data = {name, [children]}
-    //
-    //  Recursive
-
-    // This node
-    // For c in child nodes:
-    //      recurse; Returning reference to enable nodes to be linked
 }
