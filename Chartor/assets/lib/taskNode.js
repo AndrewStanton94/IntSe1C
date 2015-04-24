bfd48d05f1d3dd28f6269b4cf0e4137b34c626d5
Andrew Stanton <up683693@myport.ac.uk>
2015-04-24 15:21:39 +0100

    Some linting, and renamed <label>foo to parent

--- Chartor/assets/lib/taskNode.js
+++ Chartor/assets/lib/taskNode.js
@@ -8,10 +8,9 @@ function id(){
 		else{
 			this.key++;
 		}
-
 		return this.key;
-	}
-}
+	};
+};
 
 function Node(name){
 	this.name = name;
@@ -26,49 +25,52 @@ function Node(name){
 	}
 
 	this.buildOptions = function buildOptions(){
-	var option = '';
-	var i = this.key + "parent";
-	var optionsWrite = document.getElementById(i);
-	for (i = 0; i < nodeObjects.length; i++) { 
-		if(nodeObjects[i].key != this.key){
-    		option += '<option>' + nodeObjects[i].name + '</option>';
-		}
-		}
-		optionsWrite.innerHTML = "";
-	optionsWrite.insertAdjacentHTML('beforeend', option);
+		var option = '',
+			i = this.key + 'parent';
+			optionsWrite = document.getElementById(i);
+
+		for (i = 0; i < nodeObjects.length; i++) { 
+			if(nodeObjects[i].key != this.key){
+	    		option += '<option>' + nodeObjects[i].name + '</option>';
+				}
+			}
+			optionsWrite.innerHTML = '';
+		optionsWrite.insertAdjacentHTML('beforeend', option);
 	}
 
 	this.addNode = function addNode(){
-		this.target.insertAdjacentHTML('beforeend', '<section class="node" id="' + this.key +'"><div class="nodeTitle"><h1>'+ this.name +'</h1><p id="' + this.key +'del">Delete</p></div><div class="input"><label for="parent">foo</label><select name="parent" id="' + this.key +'parent"></select></div></section>');
+		this.target.insertAdjacentHTML('beforeend', '<section class="node" id="' + this.key +'"><div class="nodeTitle"><h1>'+ this.name +'</h1><p id="' + this.key +'del">Delete</p></div><div class="input"><label for="parent">Parent</label><select name="parent" id="' + this.key +'parent"></select></div></section>');
 	};
 
 	this.addDeleteListener = function addDeleteListener(){
-		var o = this.key + 'del';
-		var key = this.key;
-		var delBtn = document.getElementById(o);
+		var o = this.key + 'del',
+			key = this.key,
+			delBtn = document.getElementById(o);
 		
 		delBtn.addEventListener('click', function(){
 			var me = document.getElementById(key);
 			document.getElementById('inputArea').removeChild(me);
-			for(i = 0; i < nodeObjects.length; i++){
+			for(var i = 0; i < nodeObjects.length; i++){
 				if(nodeObjects[i].key == key){
 					nodeObjects.splice(i, 1);
-					for(i = 0; i < nodeObjects.length; i++){nodeObjects[i].buildOptions();}
+					for(i = 0; i < nodeObjects.length; i++){
+						nodeObjects[i].buildOptions();
+					}
 				}
 			}
 		});
-	}
+	};
 
 	this.addListener = function addListener(node, nodeTitle){
 		nodeTitle.addEventListener('click', function(){
 
-	if(node.style.height == '6.5em' || node.style.height == null){
-			node.style.height = '3em';
-		}
-	else{
-			node.style.height = '6.5em';
-		}
-	});
+			if(node.style.height == '6.5em' || node.style.height == null){
+				node.style.height = '3em';
+			}
+			else{
+				node.style.height = '6.5em';
+			}
+		});
 	};
 	
 	this.constructor = function contructor(){
@@ -76,7 +78,7 @@ function Node(name){
 		this.addDeleteListener();
 		this.addListener(this.nodeArray[this.nodeArray.length - 1], this.nodeTitleArray[this.nodeTitleArray.length - 1]);
 	}
-}
+};
 
 var keyGen = new id();
 
@@ -87,33 +89,33 @@ var nodeObjects = [];
 var node;
 
 addNode.addEventListener('click', function(){
-	if(nodeName.value != ""){
+	if(nodeName.value != ''){
 		node = new Node(nameInput.value);
 		nodeObjects.push(node);
 		node.constructor();	
-		nodeName.style.color = "black";
+		nodeName.style.color = 'black';
 		nameInput.value = '';
 	}
 	else{
-		nodeName.style.color = "red";
+		nodeName.style.color = 'red';
 	}
 });
 
-document.getElementById('nameInput').addEventListener('submit', function(e){
-	e.preventDefault();
-	if(nodeName.value != ""){
-		node = new Node(nameInput.value);
-		nodeObjects.push(node);
-		node.constructor();	
-		for (i = 0; i < nodeObjects.length; i++) { 
-			nodeObjects[i].buildOptions();
+document.getElementById('nameInput').addEventListener('submit',
+	function(e){
+		e.preventDefault();
+		if(nodeName.value != ''){
+			node = new Node(nameInput.value);
+			nodeObjects.push(node);
+			node.constructor();
+			for (i = 0; i < nodeObjects.length; i++){ 
+				nodeObjects[i].buildOptions();
+			}
+			nodeName.style.color = 'black';
+			nameInput.value = '';
 		}
-		nodeName.style.color = "black";
-		nameInput.value = '';
+		else{
+			nodeName.style.color = 'red';
+		}
 	}
-	else{
-		nodeName.style.color = "red";
-	}
-});
-
-
+);
